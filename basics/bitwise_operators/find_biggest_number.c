8f698d6281081374b29f95f10b52dcb513b1f05e
RajaKalla <gangadharkalla77@gmail.com>
2020-11-20 19:47:30 +0530

    1. 20 november 2020.

---
+++ basics/bitwise_operators/find_biggest_number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+    int amongHowManyNumbers, number1, number2, number3, position;
+    printf("How many numbers to be checked(2 or 3).\n");
+    scanf("%d", &amongHowManyNumbers);
+    if (amongHowManyNumbers == 2)
+    {
+        scanf("%d%d", &number1, &number2);
+        printf("%d", (number1 > number2) ? number1 : number2);
+    }
+    else if (amongHowManyNumbers == 3)
+    {
+        scanf("%d%d%d", &number1, &number2, &number3);
+        printf("%d", (number1 > number2 && number1 > number3) ? number1 : (number2 > number3) ? number2 : number3);
+    }
+    else
+    {
+        printf("Invalid input.");
+    }
+}
